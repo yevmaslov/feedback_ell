fb2a59a5bb277c3fdea5baa7fc2aaf46110e7227
yevheniimaslov <sqrt.evmaslov@gmail.com>
2022-12-02 21:19:13 +0200

    add current pseudo generation and ensemble

--- src/train_second_step.sh
+++ src/train_second_step.sh
@@ -26,7 +26,7 @@ do
 done
 
 
-for model_id in model67 model68 model69 model70 model71 model75 model81 model84
+for model_id in model67 model68 model69 model70
 do
   
   python train.py --config_name "${model_id}_training_config.yaml" --run_id $model_id --debug True --use_wand False --fold 0
@@ -35,6 +35,27 @@ do
   python train.py --config_name "${model_id}_training_config.yaml" --run_id $model_id --debug True --use_wand False --fold 3
   python train.py --config_name "${model_id}_training_config.yaml" --run_id $model_id --debug True --use_wand False --fold 4
   
+  python inference.py --model_dir_path "../models/${model_id}" --mode oofs --debug True
+  python inference.py --model_dir_path "../models/${model_id}" --mode prev_pseudolabels --debug True
+done
+
+
+for model_id in model21 model23 model30 model31 model37 model43 model45 model52 model67 model68 model69 model70
+do
+  python inference.py --model_dir_path "../models/${model_id}" --mode curr_pseudolabels --debug --True
+done
+python make_pseudolabels_ensemble.py --ensemble_id current_train_ensemble_4434
+
+
+for model_id in model71 model75 model81 model84
+do
+
+  python train.py --config_name "${model_id}_training_config.yaml" --run_id $model_id --debug True --use_wand False --fold 0
+  python train.py --config_name "${model_id}_training_config.yaml" --run_id $model_id --debug True --use_wand False --fold 1
+  python train.py --config_name "${model_id}_training_config.yaml" --run_id $model_id --debug True --use_wand False --fold 2
+  python train.py --config_name "${model_id}_training_config.yaml" --run_id $model_id --debug True --use_wand False --fold 3
+  python train.py --config_name "${model_id}_training_config.yaml" --run_id $model_id --debug True --use_wand False --fold 4
+
   python inference.py --model_dir_path "../models/${model_id}" --mode oofs --debug True
   python inference.py --model_dir_path "../models/${model_id}" --mode prev_pseudolabels --debug True
 done
